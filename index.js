99d1cc2ea76d92101d6621360e96ea447543585d
SandroSalzmann <salzmann2k@gmail.com>
2019-04-20 00:08:11 +0200

    Added index to export classes

---
+++ index.js
@@ -0,0 +1,15 @@
+let RTAuthentication = require("./src/RTAuthentication");
+let RTAuthenticator = require("./src/RTAuthenticator");
+let RTModifyDefinition = require("./src/RTModifyDefinition");
+let RTReadDefinition = require("./src/RTReadDefinition");
+let RTServer = require("./src/RTServer");
+let RTServerConfig = require("./src/RTServerConfig");
+
+module.exports = {
+  RTAuthentication,
+  RTAuthenticator,
+  RTModifyDefinition,
+  RTReadDefinition,
+  RTServer,
+  RTServerConfig,
+};
