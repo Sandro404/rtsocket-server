5e0ba2badad7a7124fda13cf2a7271583a1c5f25
SandroSalzmann <salzmann2k@gmail.com>
2019-04-20 18:07:20 +0200

    Added infoText attribute

--- src/RTAuthentication.js
+++ src/RTAuthentication.js
@@ -4,10 +4,12 @@ class RTAuthentication {
    * @class RTAuthentication 
    * @param {Object} queryAttributes Permitted attributes
    * @param {Number} permissions Permission level
+   * @param {String} infoText Informational text for status of authentication
    */
-  constructor(queryAttributes, permissions) {
+  constructor(queryAttributes, permissions, infoText) {
     this.queryAttributes = queryAttributes;
     this.permissions = permissions;
+    this.infoText = infoText;
   }
 }
 module.exports = RTAuthentication;
